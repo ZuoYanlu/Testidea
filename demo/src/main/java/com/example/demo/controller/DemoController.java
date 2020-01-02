83523f128a053ad2d446147be146a1fd8ff393da
zuoyanlu <1825829012@qq.com>
2020-01-03 00:22:34 +0800

    idea的gitHub测试2

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DemoController {
     @GetMapping("/hello1")
     @ResponseBody
-    public String getHello(){
+    public String getHello() {
+
         System.out.println("你好啊！！");
         return "Hello 左先生";
     }
